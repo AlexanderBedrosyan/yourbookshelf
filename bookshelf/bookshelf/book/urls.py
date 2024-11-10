b158322408c0e6e84526a6dfe63bf6fccc579bf2
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-11-10 15:42:03 +0200

    Update: 1) Home and Nav templates and 2) Book - views and urls

--- bookshelf/bookshelf/book/urls.py
+++ bookshelf/bookshelf/book/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('add-book/', views.add_book, name='add-book')
 ]
--- bookshelf/bookshelf/book/views.py
+++ bookshelf/bookshelf/book/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def add_book(request):
+    return render(request, 'book/add_book.html')
