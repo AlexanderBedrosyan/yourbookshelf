854b682823a1b2836d7c19677a4c7086797e5266
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-11-14 18:01:17 +0200

    Update author views and urls

--- bookshelf/bookshelf/author/urls.py
+++ bookshelf/bookshelf/author/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add-author/', views.add_author, name='add-author'),
-    path('<int:author_id>/author-details/', views.AuthorDetails.as_view(), name='author-details')
+    path('<int:author_id>/author-details/', views.AuthorDetails.as_view(), name='author-details'),
+    path('<int:author_id>/edit-author/', views.EditAuthorView.as_view(), name='edit-author'),
 ]
--- bookshelf/bookshelf/author/views.py
+++ bookshelf/bookshelf/author/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import DetailView
-
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, UpdateView
+from bookshelf.author.forms import UpdateAuthorForm
 from bookshelf.author.models import Author
 
 
@@ -15,3 +17,14 @@ class AuthorDetails(DetailView):
     template_name = 'author/author.html'
     model = Author
     pk_url_kwarg = 'author_id'
+
+
+class EditAuthorView(LoginRequiredMixin, UpdateView):
+    template_name = 'author/edit_author.html'
+    pk_url_kwarg = 'author_id'
+    model = Author
+    form_class = UpdateAuthorForm
+
+    def get_success_url(self):
+        author_id = self.object.id
+        return reverse_lazy('author-details', kwargs={'author_id': author_id})
