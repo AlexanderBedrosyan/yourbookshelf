12a40f51856713b0ba39e374e5a4cf1e4407e96d
AlexanderBedrosyan <126572116+AlexanderBedrosyan@users.noreply.github.com>
2024-11-27 16:42:06 +0200

    Modify admin for apps - accounts and common. Add new custom options, change some short names and create a correct rating report

--- bookshelf/bookshelf/accounts/admin.py
+++ bookshelf/bookshelf/accounts/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
-
 from bookshelf.accounts.models import CustomerModel
+from bookshelf.common.models import QuizResults
 
 
 # Register your models here.
 @admin.register(CustomerModel)
-class CustomerAdmin(admin.ModelAdmin):
-    pass
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'get_quiz_rating', 'get_roles')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active')
+    fields = ('username', 'email', 'first_name', 'last_name', 'picture_url', 'is_staff', 'is_active', 'groups', 'get_roles', 'get_quiz_rating', 'level')
+    readonly_fields = ('username', 'get_roles', 'get_quiz_rating', 'level')
+    actions = ['mark_as_staff', 'deactivate_users']
+
+    def mark_as_staff(self, request, queryset):
+        queryset.update(is_staff=True)
+        self.message_user(request, "Selected users have been marked as staff.")
+
+    mark_as_staff.short_description = "Mark selected users as staff"
+
+    def deactivate_users(self, request, queryset):
+        queryset.update(is_active=False)
+        self.message_user(request, "Selected users have been deactivated.")
+
+    deactivate_users.short_description = "Deactivate selected users"
+
+    def get_readonly_fields(self, request, obj=None):
+        if not request.user.is_superuser:
+            return self.fields
+        return self.readonly_fields
+
+    def get_roles(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+
+    get_roles.short_description = "Roles"
+
+    def get_quiz_rating(self, obj):
+        quiz_obj = QuizResults.objects.get(id=obj.id)
+        return quiz_obj.points
+
+    get_quiz_rating.short_description = "Quiz Rating Points"
+
+    def level(self, obj):
+        quiz_obj = QuizResults.objects.get(id=obj.id)
+        return quiz_obj.level
+
+    level.short_description = "Level"
--- bookshelf/bookshelf/common/admin.py
+++ bookshelf/bookshelf/common/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
-from bookshelf.common.models import Report, QuizResults
-
+from bookshelf.common.models import Report
 
 # Register your models here.
 
+
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'user', 'created_at', 'short_report_text', 'is_recent')
+    search_fields = ('title', 'user__username', 'report_text')
+    list_filter = ('created_at',)
+    fields = ('title', 'user', 'report_text', 'created_at', 'short_report_text')
+    readonly_fields = ('created_at', 'short_report_text')
+    actions = ['mark_reports_as_reviewed']
 
+    def short_report_text(self, obj):
+        return obj.report_text[:50] + "..." if len(obj.report_text) > 50 else obj.report_text
+    short_report_text.short_description = "Report Summary"
 
-@admin.register(QuizResults)
-class ReportAdmin(admin.ModelAdmin):
-    pass
+    def is_recent(self, obj):
+        from datetime import timedelta
+        from django.utils.timezone import now
+        return obj.created_at >= now() - timedelta(days=7)
+    is_recent.boolean = True
+    is_recent.short_description = "Created Recently"
+
+    def mark_reports_as_reviewed(self, request, queryset):
+        updated_count = queryset.update(report_text="[Reviewed] " + queryset.first().report_text)
+        self.message_user(request, f"{updated_count} report(s) marked as reviewed.")
+    mark_reports_as_reviewed.short_description = "Mark selected reports as reviewed"
+
